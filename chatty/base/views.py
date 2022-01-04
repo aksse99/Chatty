cbbd9ab457d46302f5864dd14349145cbc675b3a
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-04 17:32:45 +0530

    Code updated

--- chatty/base/views.py
+++ chatty/base/views.py
@@ -1,22 +1,14 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': 'Lets learn python!'},
-    {'id': 2, 'name': 'Design with me'},
-    {'id': 3, 'name': 'Frontend developers'},
-
-]
+from .models import Room
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
