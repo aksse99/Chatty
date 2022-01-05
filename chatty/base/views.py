4595cf581d054f687db42731b5bd84ac45517fe8
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-05 20:04:24 +0530

    changes

--- chatty/base/views.py
+++ chatty/base/views.py
@@ -30,7 +30,21 @@ def createRoom(request):
 
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
-    form - RoomForm(instance=room)
+    form = RoomForm(instance=room)
 
-    context = {}
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room})
