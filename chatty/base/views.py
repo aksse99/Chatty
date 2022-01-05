96e4ace41a6a14adfdd50bfa66158fb7365e9b72
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-05 14:06:55 +0530

    updates

--- chatty/base/views.py
+++ chatty/base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 
 def home(request):
@@ -12,3 +13,24 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+
+def createRoom(request):
+
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form - RoomForm(instance=room)
+
+    context = {}
+    return render(request, 'base/room_form.html', context)
