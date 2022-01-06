e87418b2ad192ddcda8e9700b0df3e6e84102ba0
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-06 21:27:26 +0530

    updates

--- chatty/base/views.py
+++ chatty/base/views.py
@@ -18,7 +18,7 @@ def loginPage(request):
         return redirect("home")
 
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -44,8 +44,20 @@ def logoutUser(request):
 
 
 def registerPage(request):
-    page = "register"
+
     form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An error occurred during registration")
+
     return render(request, "base/login_register.html", {"form": form})
 
 
