81f8be177b80ee55d805a55d7f053385d4453a8b
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-08 21:17:21 +0530

    added template

--- chatty/chatty/settings.py
+++ chatty/chatty/settings.py
@@ -120,6 +120,8 @@ STATIC_URL = "static/"
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
+# STATIC_ROOT =
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
