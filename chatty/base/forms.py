ef7d5e9ea7da60a423a84d169e5dc57a7d1a5d2f
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-08 20:07:41 +0530

    static file added

--- chatty/base/forms.py
+++ chatty/base/forms.py
@@ -5,4 +5,5 @@ from .models import Room
 class RoomForm(ModelForm):
     class Meta:
         model = Room
-        fields = '__all__'
+        fields = "__all__"
+        exclude = ["host", "participants"]
