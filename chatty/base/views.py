d029444cae2b48d039ef788297c09b41a13a48af
Ayush <57259393+aksse99@users.noreply.github.com>
2022-01-10 20:19:28 +0530

    updated profile page

--- chatty/base/views.py
+++ chatty/base/views.py
@@ -7,7 +7,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from .models import Room, Topic, Message
 from django.contrib.auth.forms import UserCreationForm
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 
 
 def loginPage(request):
@@ -186,3 +186,17 @@ def deleteMessage(request, pk):
         message.delete()
         return redirect("home")
     return render(request, "base/delete.html", {"obj": message})
+
+
+@login_required(login_url="login")
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == "POST":
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect("user-profile", pk=user.id)
+
+    return render(request, "base/update-user.html", {"form": form})
